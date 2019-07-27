762e6874c8eb533afcf31bc3aedd657cfd9d4ab0
stefaan himpe <stefaan.himpe@gmail.com>
2019-07-27 17:45:44 +0200

    refactoring: create a plotter server to keep ui responsive during gcode sending

--- src/gcodesendertab/gcodesendertab.py
+++ src/gcodesendertab/gcodesendertab.py
@@ -1,13 +1,8 @@
 from tab import Tab
-from os import linesep
 from os.path import expanduser
-from serial import Serial
 from serial.tools.list_ports import comports
-from serial.serialutil import SerialException
-from serial import serial_for_url
-from PyQt5.QtWidgets import QMessageBox
-import time
-import io
+from os import linesep
+from gcodesendertab.plotterserver import PlotterServer
 
 SETTLING_TIME = 2.0
 TIMEOUT = 10
@@ -16,29 +11,66 @@ class GcodeSenderTab(Tab):
     def __init__(self, parent=None, itemsPerLayer=None):
         super().__init__(parent, itemsPerLayer)
         self.homeFolder = expanduser("~")
-        self.item_to_device = {}
-        self.serial = None
-        self.sio = None
-
+        self.server = PlotterServer(self.parent.cmdFinishedGcodeSender.text().strip())
         self.paused = False
-        self.canceled = False
+        self.item_to_device = {}
 
     def setupSlots(self):
         self.parent.refreshPortsGcodeSender.clicked.connect(self.OnRefreshPorts)
         self.parent.portGcodeSender.currentTextChanged.connect(self.OnSelectPort)
         self.parent.sendRawTextGcodeSender.returnPressed.connect(self.OnSendRawCommand)
-        self.parent.sendTaskGcodeSender.currentTextChanged.connect(self.OnSendTask)
+        self.parent.sendTaskGcodeSender.activated.connect(self.OnSendTask)
         self.EnableDisableSendControls(False)
         self.parent.pauseGcodeSender.clicked.connect(self.OnPauseCode)
         self.parent.cancelGcodeSender.clicked.connect(self.OnCancelCode)
         self.OnRefreshPorts()
 
+        self.server.enabledisable_send_controls.connect(self.EnableDisableSendControls)
+        self.server.log.connect(self.Log)
+        self.server.on_pause.connect(self.OnPauseServer)
+        self.server.on_resume.connect(self.OnResumeServer)
+        self.server.on_cancel.connect(self.OnCancelServer)
+        self.server.on_killed.connect(self.OnKilledServer)
+        self.server.on_queuesize_changed.connect(self.OnQueueSizeChanged)
+
+    def OnQueueSizeChanged(self, newsize):
+        self.parent.queueSizeGcodeSender.setText("{0}".format(newsize))
+
+    def Log(self, msg):
+        if not msg.endswith(linesep):
+            msg += linesep
+        #print(msg)
+        self.parent.serialMonitorGcodeSender.insertPlainText(msg)
+        self.parent.serialMonitorGcodeSender.ensureCursorVisible()
+
+    def OnKilledServer(self):
+        self.Log("[Server] Plotter server killed.")
+
+    def OnConnected(self, success):
+        if success:
+            self.Log("[Server] Machine ready.")
+        else:
+            self.Log("[Server] Machine didn't initialize correctly.")
+
     def OnPauseCode(self):
-        self.paused = not self.paused
-        self.parent.pauseGcodeSender.setText("Resume" if self.paused else "Pause")
+        if self.parent.pauseGcodeSender.text() == "Pause":
+            self.server.pause()
+        else:
+            self.server.resume()
+
+    def OnPauseServer(self):
+        self.parent.pauseGcodeSender.setText("Resume")
+        self.Log("[Server] Pause program execution. Operation in progress will still complete.")
+
+    def OnResumeServer(self):
+        self.parent.pauseGcodeSender.setText("Pause")
+        self.Log("[Server] Resume program execution.")
 
     def OnCancelCode(self):
-        self.canceled = True
+        self.server.cancel()
+
+    def OnCancelServer(self):
+        self.Log("[Server] Cancel program execution. Operation in progress will still complete.")
 
     def OnRefreshPorts(self):
         ports = comports()
@@ -58,8 +90,7 @@ class GcodeSenderTab(Tab):
             self.parent.portGcodeSender.addItem(i)
 
         # close existing connection if any
-        if self.serial and self.serial.is_open:
-            self.serial.close()
+        self.server.close()
 
         # select "not connected" by default
         self.parent.portGcodeSender.setCurrentText("Not connected")
@@ -72,132 +103,45 @@ class GcodeSenderTab(Tab):
         self.parent.sendTaskGcodeSender.setEnabled(enable)
 
     def OnSelectPort(self, newport):
-        startstring = self.parent.initFinishedGcodeSender.text().strip()
-        baudrate = int(self.parent.baudRateGcodeSender.currentText())
-        self.EnableDisableSendControls(False)
-        if newport == "":
-            return
-        if newport == "Not connected":
-            if self.serial and self.serial.is_open:
-                self.serial.close()
-            return
-        if newport in self.item_to_device:
-            device = self.item_to_device[newport]
-            self.serial = serial_for_url(device)
-            if self.serial and self.serial.is_open:
-                self.serial.close()
-            self.serial.port = device
-            self.serial.timeout = TIMEOUT
-            self.serial.baudrate = baudrate
-            try:
-                self.serial.open()
-                time.sleep(SETTLING_TIME) # sleep a while before accessing the port
-                if self.serial.isOpen():
-                    self.serial_is_open = True
-                    self.EnableDisableSendControls(True)
-                    self.parent.serialMonitorGcodeSender.insertPlainText("Serial port {0} opened successfully.{1}Waiting for initialization to complete.{1}".format(newport, linesep))
-                    started = False
-                    while not started:
-                        time.sleep(0.1)
-                        readval = b''
-                        while self.serial.inWaiting() > 0:
-                            readval += self.serial.read(1)
-                        if readval:
-                            #print(readval)
-                            reply = str(readval, "utf-8").splitlines()
-                            for r in reply:
-                                self.parent.serialMonitorGcodeSender.insertPlainText("> {0}{1}".format(r, linesep))
-                                self.parent.application.processEvents()
-                                if startstring  in r:
-                                    started = True
-                    self.parent.serialMonitorGcodeSender.insertPlainText("> Machine initialized."+linesep)
-                    self.sio = io.TextIOWrapper(io.BufferedRWPair(self.serial, self.serial, 1))
-                else:
-                    msg_box = QMessageBox()
-                    msg_box.setText("Error opening serial port. Unknown reason.");
-                    msg_box.exec()
-
-            except SerialException as e:
-                self.serial_is_open = False
-                self.EnableDisableSendControls(False)
-                msg_box = QMessageBox()
-                msg_box.setText("Error opening serial port: {0}".format(e.strerror));
-                msg_box.exec()
-        else:
-            print("newport = '{0}' not present in {1}".format(newport, self.item_to_device.keys()))
-
-    def write_to_serial(self, cmd):
-        cmd = cmd.split("(")[0]  # cut off comments
-        cmd = cmd.split(";")[0]  # cut off comments
-        cmd = cmd.strip().upper()
-        if cmd:
-            okstring = self.parent.cmdFinishedGcodeSender.text().strip()
-            if self.serial_is_open:
-                cmd = cmd + linesep
-                self.parent.serialMonitorGcodeSender.insertPlainText("< " + cmd)
-                self.parent.application.processEvents()
-                self.sio.write(cmd)
-                self.sio.flush()  # it is buffering. required to get the data out *now*
-                ok = False
-                maxattempts = 10000
-                attempt = 0
-                while not ok and attempt < maxattempts:
-                    time.sleep(0.01)
-                    readval = ''
-                    while self.serial.inWaiting() > 0:
-                        readval += self.serial.readline().decode("ascii")
-                    if readval:
-                        #print(readval)
-                        reply = readval.splitlines()
-                        for r in reply:
-                            self.parent.serialMonitorGcodeSender.insertPlainText("> {0}{1}".format(r, linesep))
-                            self.parent.serialMonitorGcodeSender.ensureCursorVisible()
-                            self.parent.application.processEvents()
-                            if okstring in r:
-                                ok = True
-                if not ok:
-                    self.parent.serialMonitorGcodeSender.insertPlainText("ERROR: TIMEOUT?!")
-                    self.parent.application.processEvents()
-            else:
-                self.parent.serialMonitorGcodeSender.insertPlainText("ERROR! Serial port not open." + linesep)
+        if newport and newport != "Not connected":
+            startstring = self.parent.initFinishedGcodeSender.text().strip()
+            baudrate = int(self.parent.baudRateGcodeSender.currentText())
+            self.Log("[UI] Establishing machine connection. Please wait until finished. UI will be unresponsive during init.")
+            self.EnableDisableSendControls(False)
+            self.parent.application.processEvents()
+            self.server.connect(newport, baudrate, self.item_to_device, startstring)
+            self.server.start()
+        elif newport == "Not connected":
+            self.server.kill()
 
     def OnSendRawCommand(self):
         cmd = self.parent.sendRawTextGcodeSender.text().strip()
-        self.write_to_serial(cmd)
+        self.server.submit(cmd)
 
-    def OnSendTask(self):
-        cmd = self.parent.sendTaskGcodeSender.currentText()
+    def OnSendTask(self, cmdidx):
+        cmd = self.parent.sendTaskGcodeSender.itemText(cmdidx)
         task_to_code = {
             'No task selected' : '',
             'Pen up' : 'G00 {0} F{1}'.format(self.parent.penUpCmdGcode.text(), self.parent.penDownSpeedGcode.text()),
             'Pen down': 'G00 {0} F{1}'.format(self.parent.penDownCmdGcode.text(), self.parent.penDownSpeedGcode.text()),
             'Go to 0,0 (fast)' : "G00 X0 Y0",
             'Home (slowly, more accurate)' : "G00 X0 Y0|G28 X Y",
-            'Move along page outline with pen up' : "G00 {0} F{1}|G00 X0 Y0|G28 X Y|G01 X{2} Y0 F{3}|G01 X{2} Y{4}|G01 X0 Y{4}|G01 X0 Y0".format(
+            'Move along page outline with pen up' : "G00 {0} F{1}|G00 X0 Y0|G01 X{2} Y0 F{3}|G01 X{2} Y{4}|G01 X0 Y{4}|G01 X0 Y0".format(
                 self.parent.penUpCmdGcode.text(),
                 self.parent.penDownSpeedGcode.text(),
-                self.parent.pageWidthGcode.text(),
+                self.parent.pageWidthGcode.text().replace("M","").replace("m",""),
                 self.parent.drawingSpeedGcode.text(),
-                self.parent.pageHeightGcode.text()
+                self.parent.pageHeightGcode.text().replace("M","").replace("m","")
             ),
             'Draw page outline (set up in Gcode generation tab)' : "G00 {0} F{1}|G00 X0 Y0|G28 X Y|G01 X{2} Y0 F{3}|G01 X{2} Y{4}|G01 X0 Y{4}|G01 X0 Y0".format(
                 self.parent.penDownCmdGcode.text(),
                 self.parent.penDownSpeedGcode.text(),
-                self.parent.pageWidthGcode.text(),
+                self.parent.pageWidthGcode.text().replace("M","").replace("m",""),
                 self.parent.drawingSpeedGcode.text(),
-                self.parent.pageHeightGcode.text()
+                self.parent.pageHeightGcode.text().replace("M","").replace("m","")
             ),
         }
         if cmd in task_to_code:
             code = task_to_code[cmd].split("|")
             for c in code:
-                while self.paused:
-                    self.parent.application.processEvents()
-                    time.sleep(0.01)
-                    if self.canceled:
-                        self.OnPauseCode() # untoggle pause button again
-                        break
-                if self.canceled:
-                    self.canceled = False
-                    break
-                self.write_to_serial(c)
+                self.server.submit(c)
---
+++ src/gcodesendertab/plotterserver.py
@@ -0,0 +1,170 @@
+from PyQt5.QtCore import QObject, pyqtSignal
+from serial.serialutil import SerialException
+from serial import serial_for_url
+from time import sleep
+from os import linesep
+import io
+from queue import Queue, Empty
+from threading import Thread
+
+SETTLING_TIME = 2.0
+TIMEOUT = 10
+
+class PlotterServer(QObject):
+    enabledisable_send_controls = pyqtSignal(bool)
+    log = pyqtSignal(str)
+    on_pause = pyqtSignal()
+    on_resume = pyqtSignal()
+    on_cancel = pyqtSignal()
+    on_killed = pyqtSignal()
+    on_connected = pyqtSignal(bool)
+    on_open_port = pyqtSignal()
+    on_close_port = pyqtSignal()
+    on_queuesize_changed = pyqtSignal(int)
+
+    def __init__(self, okstring):
+        super().__init__()
+        self.serial = None
+        self.sio = None
+        self.paused = False
+        self.canceled = False
+        self.queue = Queue()
+        self.okstring = okstring
+        self.started = False
+        self.killed = False
+
+    def kill(self):
+        self.killed = True
+
+    def start(self):
+        if not self.started:
+            self.killed =  False
+            self.canceled = False
+            self.paused = False
+            self.started = True
+            self.thread = Thread(target=self.process, args=(self.queue,))
+            self.log.emit("[Server] Starting server...")
+            self.thread.start()
+            self.log.emit("[Server] Server up and running!")
+
+    def submit(self, cmd):
+        self.queue.put(cmd)
+        self.on_queuesize_changed.emit(self.queue.qsize())
+
+    def close(self):
+        if self.serial and self.serial.is_open:
+            self.serial.close()
+            self.on_close_port.emit()
+
+    def process(self, queue):
+        while not self.killed:
+            while not self.canceled:
+                if self.serial:
+                    while self.paused:
+                        sleep(0.1)
+                        if self.canceled:
+                            break
+                    cmd = queue.get()
+                    self.on_queuesize_changed.emit(self.queue.qsize())
+                    self.write_to_serial(cmd, self.okstring)
+                    queue.task_done()
+            if self.canceled:
+                while not queue.empty():
+                    try:
+                        queue.get(False)
+                    except Empty:
+                        continue
+                    queue.task_done()
+                    self.on_queuesize_changed.emit(self.queue.qsize())
+                self.canceled = False
+
+        self.close()
+        self.on_killed.emit()
+
+    def cancel(self):
+        self.canceled = True
+        self.on_cancel.emit()
+
+    def pause(self):
+        self.paused = True
+        self.on_pause.emit()
+
+    def resume(self):
+        self.paused = False
+        self.on_resume.emit()
+
+    def connect(self, port, baudrate, item_to_device, init_finished_string):
+        if port == "":
+            return
+        if port == "Not connected":
+            self.close()
+            return
+        if port in item_to_device:
+            device = item_to_device[port]
+            self.serial = serial_for_url(device)
+            self.close()
+            self.serial.port = device
+            self.serial.timeout = TIMEOUT
+            self.serial.baudrate = baudrate
+            try:
+                self.serial.open()
+                sleep(SETTLING_TIME) # sleep a while before accessing the port
+                if self.serial.isOpen():
+                    self.serial_is_open = True
+                    self.on_open_port.emit()
+                    self.enabledisable_send_controls.emit(True)
+                    self.log.emit("[Server] Serial port {0} opened successfully.{1}Waiting for initialization to complete.{1}".format(port, linesep))
+                    started = False
+                    while not started:
+                        sleep(0.1)
+                        readval = b''
+                        while self.serial.inWaiting() > 0:
+                            readval += self.serial.read(1)
+                        if readval:
+                            #print(readval)
+                            reply = str(readval, "utf-8").splitlines()
+                            for r in reply:
+                                self.log.emit("[Server] > {0}{1}".format(r, linesep))
+                                if init_finished_string  in r:
+                                    started = True
+                    self.sio = io.TextIOWrapper(io.BufferedRWPair(self.serial, self.serial, 1))
+                    self.on_connected.emit(True)
+                else:
+                    self.log.emit("[Server] Error opening serial port. Unknown reason.")
+
+            except SerialException as e:
+                self.serial_is_open = False
+                self.on_connected.emit(False)
+                self.enabledisable_send_controls(False)
+                self.log.emit("[Server] Error opening serial port: {0}".format(e.strerror))
+        else:
+            print("[Server] port = '{0}' not present in {1}".format(port, item_to_device.keys()))
+
+    def write_to_serial(self, cmd, okstring):
+        cmd = cmd.split("(")[0]  # cut off comments
+        cmd = cmd.split(";")[0]  # cut off comments
+        cmd = cmd.strip().upper()
+        if cmd:
+            if self.serial_is_open:
+                cmd = cmd + linesep
+                self.log.emit("[Server] < " + cmd)
+                self.sio.write(cmd)
+                self.sio.flush()  # it is buffering. required to get the data out *now*
+                ok = False
+                maxattempts = 10000
+                attempt = 0
+                while not ok and attempt < maxattempts:
+                    sleep(0.01)
+                    readval = ''
+                    while self.serial.inWaiting() > 0:
+                        readval += self.serial.readline().decode("ascii")
+                    if readval:
+                        reply = readval.splitlines()
+                        for r in reply:
+                            self.log.emit("[Server] > {0}{1}".format(r, linesep))
+                            if okstring in r:
+                                ok = True
+                if not ok:
+                    self.log.emit("[Server] ERROR: TIMEOUT waiting for reply reported by plotter server")
+            else:
+                self.log.emit("[Server] ERROR! Serial port not open." + linesep)
