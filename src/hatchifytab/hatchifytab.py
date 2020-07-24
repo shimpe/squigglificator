2d67ba47eeed10ebea4bbca4651d27d88c7dae35
stefaan himpe <stefaan.himpe@gmail.com>
2020-07-24 15:50:51 +0200

    updates to the hatchify method: expose more parameters, change the way it works a bit

--- src/hatchifytab/hatchifytab.py
+++ src/hatchifytab/hatchifytab.py
@@ -10,6 +10,8 @@ from imageconvert import qimage_to_pil_image
 from PIL import Image, ImageOps
 import cv2
 import noise
+from simplification.cutil import simplify_coords
+
 
 class HatchifyTab(Tab):
     def __init__(self, parent=None, layersModel=None):
@@ -44,10 +46,13 @@ class HatchifyTab(Tab):
         called when user clicks set defaults button
         :return:
         """
-        self.parent.includeContoursHatchify.setValue(1)
-        self.parent.includeHatchingHatchify.setValue(1)
+        self.parent.includeContoursHatchify.setCheckState(Qt.Checked)
+        self.parent.includeHatchingHatchify.setCheckState(Qt.Checked)
         self.parent.hatchSizeHatchify.setValue(16)
         self.parent.contourSimplificationHatchify.setValue(2)
+        self.parent.maxGVHorizontalHatchingHatchify.setValue(144)
+        self.parent.maxGVDenseHorizontalHatchingHatchify.setValue(64)
+        self.parent.maxGVCrossHatchingHatchify.setValue(16)
         layerId = self.parent.layersList.currentIndex()
         self.parent.layersModel.itemFromIndex(layerId).setCheckState(Qt.Checked)
 
@@ -60,10 +65,13 @@ class HatchifyTab(Tab):
         summarize ui controls in a model (dict of key->value)
         :return: model
         """
-        model = {'includeContours'        : int(self.parent.includeContoursHatchify.checkState() == Qt.Checked),
-                 'includeHatching' : int(self.parent.includeHatchingHatchify.checkState() == Qt.Checked),
+        model = {'includeContours'        : int(self.parent.includeContoursHatchify.checkState()),
+                 'includeHatching' : int(self.parent.includeHatchingHatchify.checkState()),
                  'hatchSize'    : self.parent.hatchSizeHatchify.value(),
                  'contourSimplification'    : self.parent.contourSimplificationHatchify.value(),
+                 'horGVHatching' : self.parent.maxGVHorizontalHatchingHatchify.value(),
+                 'denseHorGVHatching' : self.parent.maxGVDenseHorizontalHatchingHatchify.value(),
+                 'crossGVHatching' : self.parent.maxGVCrossHatchingHatchify.value()
                  }
         return model
 
@@ -77,6 +85,9 @@ class HatchifyTab(Tab):
         self.parent.includeHatchingHatchify.setCheckState(int(model['includeHatching']))
         self.parent.hatchSizeHatchify.setValue(int(model['hatchSize']))
         self.parent.contourSimplificationHatchify.setValue(int(model['contourSimplification']))
+        self.parent.maxGVHorizontalHatchingHatchify.setValue(int(model['horGVHatching']))
+        self.parent.maxGVDenseHorizontalHatchingHatchify.setValue(int(model['denseHorGVHatching']))
+        self.parent.maxGVCrossHatchingHatchify.setValue(int(model['crossGVHatching']))
 
     def find_edges(self, IM):
         im = np.array(IM)
@@ -157,14 +168,16 @@ class HatchifyTab(Tab):
         contours = [c for c in contours if len(c) > 1]
 
         for i in range(0, len(contours)):
-            contours[i] = [(v[0] * contour_simplify, v[1] * contour_simplify) for v in contours[i]]
+            contours[i] = [(v[0], v[1]) for v in contours[i]]
 
         for i in range(0, len(contours)):
             for j in range(0, len(contours[i])):
                 contours[i][j] = int(contours[i][j][0] + 10 * noise.pnoise3(i * 0.5, j * 0.1, 1)), int(
                     contours[i][j][1] + 10 * noise.pnoise3(i * 0.5, j * 0.1, 2))
 
-        return contours
+        simple_contours = [simplify_coords(c, contour_simplify) for c in contours]
+
+        return simple_contours
 
     def distsum(self, *args):
         return sum([
@@ -173,43 +186,103 @@ class HatchifyTab(Tab):
 
     def hatch(self, IM, sc=16):
         PX = IM.load()
-        w, h = IM.size
-        lg1 = []
-        lg2 = []
-        for x0 in range(w):
-            for y0 in range(h):
-                x = x0 * sc
-                y = y0 * sc
-                if PX[x0, y0] > 144:
-                    pass
+        width, height = IM.size
+        lines = []
 
-                elif PX[x0, y0] > 64:
-                    lg1.append([(x, y + sc / 4), (x + sc, y + sc / 4)])
-                elif PX[x0, y0] > 16:
-                    lg1.append([(x, y + sc / 4), (x + sc, y + sc / 4)])
-                    lg2.append([(x + sc, y), (x, y + sc)])
+        maxgvhor = self.parent.maxGVHorizontalHatchingHatchify.value()
+        maxgvdensehor = self.parent.maxGVDenseHorizontalHatchingHatchify.value()
+        maxgvcross = self.parent.maxGVCrossHatchingHatchify.value()
 
+        # horizontal hatches
+        for y in range(0, height, sc):
+            pendown = False
+            for x in range(0, width, sc):
+                p = PX[x,y]
+                if p <= maxgvhor:
+                    if not pendown:
+                        lines.append([[x,y]])
+                    else:
+                        lines[-1].append([x,y])
+                    pendown = True
                 else:
-                    lg1.append([(x, y + sc / 4), (x + sc, y + sc / 4)])
-                    lg1.append([(x, y + sc / 2 + sc / 4), (x + sc, y + sc / 2 + sc / 4)])
-                    lg2.append([(x + sc, y), (x, y + sc)])
+                    pendown = False
 
-        lines = [lg1, lg2]
-        for k in range(0, len(lines)):
-            for i in range(0, len(lines[k])):
-                for j in range(0, len(lines[k])):
-                    if lines[k][i] != [] and lines[k][j] != []:
-                        if lines[k][i][-1] == lines[k][j][0]:
-                            lines[k][i] = lines[k][i] + lines[k][j][1:]
-                            lines[k][j] = []
-            lines[k] = [l for l in lines[k] if len(l) > 0]
-        lines = lines[0] + lines[1]
+        # denser horizontl
+        for y in range(sc//2, height, sc):
+            pendown = False
+            for x in range(0, width, sc):
+                if PX[x,y] <= maxgvdensehor:
+                    if not pendown:
+                        lines.append([[x,y]])
+                    else:
+                        lines[-1].append([x,y])
+                    pendown = True
+                else:
+                    pendown = False
 
-        for i in range(0, len(lines)):
-            for j in range(0, len(lines[i])):
-                lines[i][j] = int(lines[i][j][0] + sc * noise.pnoise3(i * 0.5, j * 0.1, 1)), int(
-                    lines[i][j][1] + sc * noise.pnoise3(i * 0.5, j * 0.1, 2)) - j
+        # diagonal top-left
+        for sy in range(0, height, sc):
+            pendown = False
+            for x,y in zip(range(0, width, sc), range(sy-1, -1, -sc)):
+                if PX[x,y] <= maxgvcross:
+                    if not pendown:
+                        lines.append([[x,y]])
+                    else:
+                        lines[-1].append([x,y])
+                    pendown = True
+                else:
+                    pendown = False
+
+        # for fun top left to bottom right
+        #for sy in range(0, height, sc):
+        #    pendown = False
+        #    for x,y in zip(range(0, width, sc), range(0, sy, sc)):
+        #        if PX[x,y] <= maxgvcross:
+        #            if not pendown:
+        #                lines.append([[x,y]])
+        #            else:
+        #                lines[-1].append([x,y])
+        #            pendown = True
+        #        else:
+        #            pendown = False
+
+        # diagonal bottom right
+        for sx in range(0, width, sc):
+            pendown = False
+            for x,y in zip(range(sx, width, sc), range(height-1, -1, -sc)):
+                #print("x = {0}, y = {1}".format(x,y))
+                if PX[x,y] <= maxgvcross:
+                    if not pendown:
+                        lines.append([[x,y]])
+                    else:
+                        lines[-1].append([x,y])
+                    pendown = True
+                else:
+                    pendown = False
+
+        # for fun topleft to bottomright
+        #for sx in range(0, width, sc):
+        #    pendown = False
+        #    for x,y in zip(range(sx, width, sc), range(0, height, sc)):
+        #        #print("x = {0}, y = {1}".format(x,y))
+        #        if PX[x,y] <= maxgvcross:
+        #            if not pendown:
+        #                lines.append([[x,y]])
+        #            else:
+        #                lines[-1].append([x,y])
+        #            pendown = True
+        #        else:
+        #            pendown = False
+
+        self.add_noise(lines, sc)
+
         return lines
+
+    def add_noise(self, lines, sc):
+        for i in range(len(lines)):
+            for j in range(len(lines[i])):
+                lines[i][j][0] = (lines[i][j][0] + sc * noise.pnoise3(i * 0.5, j * 0.1, 1))
+                lines[i][j][1] = (lines[i][j][1] + sc * noise.pnoise3(i * 0.5, j * 0.1, 1))
 
     def sortlines(self, lines):
         clines = lines[:]
@@ -238,7 +311,7 @@ class HatchifyTab(Tab):
         :param image: bitmap to squigglify
         :return:
         """
-        print("image width = {0}, image height = {1}".format(image.width(), image.height()))
+        #print("image width = {0}, image height = {1}".format(image.width(), image.height()))
         ctimage = ImageOps.autocontrast(qimage_to_pil_image(image).convert("L"), 10)
         draw_contours = self.parent.includeContoursHatchify.checkState() == Qt.Checked
         draw_hatch = self.parent.includeHatchingHatchify.checkState() == Qt.Checked
@@ -251,13 +324,9 @@ class HatchifyTab(Tab):
         lines = []
         inv_resize_factor = (1, 1)
         if draw_contours:
-            contour_resize_factor = (resolution // contour_simplify, int(resolution * aspect_ratio) // contour_simplify)
-            print("contour resize factor {0}".format(contour_resize_factor))
-            lines += self.getcontours(ctimage.resize(contour_resize_factor), contour_simplify)
+            lines += self.getcontours(ctimage, contour_simplify)
         if draw_hatch:
-            hatch_resize_factor = (resolution // hatch_size, int(resolution * aspect_ratio)//hatch_size)
-            print("hatch resize factor {0}".format(hatch_resize_factor))
-            lines += self.hatch(ctimage.resize(hatch_resize_factor), hatch_size)
+            lines += self.hatch(ctimage, hatch_size)
 
         lines = self.sortlines(lines)
 
